d63d5aaf57f80eeeb87a25e4e54003c2c10993f4
Phil Tysoe <philtysoe@gmail.com>
2012-02-19 16:36:41 +0000

    Can now add rst posts from files or links

--- portfolio/blog/models.py
+++ portfolio/blog/models.py
@@ -1,3 +1,5 @@
+import urllib2
+
 from django.db import models
 
 from tagging.fields import TagField
@@ -16,10 +18,12 @@ class PostBase(TimeStampedModel):
 
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=255)
-    content = tinymce_models.HTMLField()
+    content = tinymce_models.HTMLField(blank=True, help_text='For RST you can optionally leave this blank and source your RST via a link OR file')
     slug = models.SlugField(unique=True)
     tags = TagField()
     format = models.CharField(max_length=2, choices=FORMATS, default='RS')
+    link = models.URLField(blank=True, help_text='Link to RST. For example \'https://raw.github.com/igniteflow/django-portfolio/master/README.rst\'')
+    file = models.FileField(upload_to='posts', help_text='Your RST file to be stored as HTML')
  
     def get_tags(self):
         return Tag.objects.get_for_object(self)
@@ -32,10 +36,25 @@ class PostBase(TimeStampedModel):
         result = core.publish_parts(content, writer=w)['html_body']
         return '\n'.join(result.split('\n')[1:-2])
 
+    def get_web_resource(self, link):
+        return urllib2.urlopen(link).read()
+
+    def get_file_resources(self, file):
+        pass
+
+    def is_new_rst_resource(self):
+        return not self.id and self.format == 'RS'
+
     def save(self, *args, **kwargs):
-        if self.format == 'RS':
+        if self.is_new_rst_resource():
+            # @todo add form validation to only allow either link or file, not both
+            if self.link:
+                self.content = self.get_web_resource(self.link)
+            elif self.file:
+                self.content = self.file.read()
             self.content = self.rst2html(self.content)
         super(PostBase, self).save(*args, **kwargs)
 
     def __unicode__(self):
         return self.title
+
